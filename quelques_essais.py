a303a4a4385dd0847a3e8f0523482cea44d94fc8
Thierryde <thierry.delongueville@gmail.com>
2016-03-07 20:03:42 +0100

    ajout de la fonction division

--- quelques_essais.py
+++ quelques_essais.py
@@ -1,3 +1,9 @@
 def carre(a):
-		a = a * a
-		print (a)
+	a = a * a
+	print (a)
+        
+def division(a,b):
+	result = a / b
+	reste = a % b
+	print "le resultat de %d divise par %d est %d et il reste %d" % (a, b, result, reste)
+        
