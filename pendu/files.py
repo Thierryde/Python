bc005f05f97742d4250738496a1b0c5f3bd40a05
Thierryde <thierry.delongueville@gmail.com>
2016-03-08 01:55:21 +0100

    push du code de base pour le jeu pendu, non fonctionnel

---
+++ pendu/files.py
@@ -0,0 +1,18 @@
+# contient donnees utiles au jeu "pendu"
+
+nb_vies = 8
+# nombre de vies par partie
+
+nom_fichier_scores = "scores"
+# nom du fichier contenant les scores
+
+liste_mots = [
+	"asticot",
+	"abricot",
+	"billard",
+	"bateau",
+	"cochon",
+	"chien",
+	"daurade",
+	"elephant",
+]
